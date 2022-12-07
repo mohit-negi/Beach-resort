f886865e205746734ab650ef32fc379dfc59339f
ArduousEndeavour <mohitneg377@gmail.com>
2022-12-07 19:45:36 +0530

    Added banner component

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,9 +1,13 @@
 import React,{Component} from "react"
 import Hero from '../components/Hero'
-export class Error extends Component{
-    render(){
-        return(
-            <Hero/>
-        )
-    }
+import Banner from "../components/Banner"
+import { Link } from "react-router-dom"
+export function Error(){
+    return(
+        <Hero>
+            <Banner title="Error 404" subtitle="Page not found">
+                <Link to="/" className="btn-primary">Home</Link>
+            </Banner>
+        </Hero>
+    )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,19 @@
 import React,{Component} from "react"
+import Hero from "../components/Hero"
+import Banner from "../components/Banner"
+import { Link } from "react-router-dom"
+
 export default class Home extends Component{
     render(){
         return(
-            <div>
+            <Hero>
+                <Banner title="luxurious rooms" subtitle="deluxe rooms starting at Just ₹5000">
+                <Link to='/rooms' className="btn-primary">
+                    Our rooms
+                </Link>
+                </Banner>
                 
-            </div>
+            </Hero>
         )
     }
 }
